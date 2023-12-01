bd10537c3a8af4cad0b34450b907ad341d5c0776
Will Turner <will@thefutureforestcompany.com>
2023-12-01 23:56:03 +0000

    54083 is too low, 54098 is too high

--- day01/main.cpp
+++ day01/main.cpp
@@ -3,10 +3,13 @@
 #include <string>
 #include <stdio.h>
 #include <ctype.h>
+#include <map>
 
 using namespace std;
 
-int main() {
+string replaceStringWithNum(string line);
+
+int main(){
 
   std::cout << "Opening file... \n";
 
@@ -14,41 +17,79 @@ int main() {
   input_file.open("inputs.txt", ios::in);
 
   int running_total = 0;
-  
+
   if (input_file.is_open()){
     string line;
 
-    while(getline(input_file, line)){
+    while (getline(input_file, line)){
       string firstChar;
       bool firstFound = false;
       string lastChar;
       bool lastFound = false;
-      
+
+      // replace the text with digits then continue
+      line = replaceStringWithNum(line);
+
       for (int i = 0; i < line.length(); i++){
-	if (!firstFound){
-	  if (isdigit(line[i])){
-	    firstChar = line[i];
-	    firstFound = true;
-	  }
-	}
+        if (!firstFound){
+          if (isdigit(line[i])){
+            firstChar = line[i];
+            firstFound = true;
+          }
+        }
       }
 
-      for (int i = line.length() -1 ; i >= 0; i--){
-	if (!lastFound){	
-	  if (isdigit(line[i])){
-	    lastChar = line[i];
-	    lastFound = true;
-	  }
-	}
+      for (int i = line.length() - 1; i >= 0; i--){
+        if (!lastFound){
+          if (isdigit(line[i])){
+            lastChar = line[i];
+            lastFound = true;
+          }
+        }
       }
 
       string resulting_string = firstChar + lastChar;
       int resulting_num = std::stoi(resulting_string);
-      running_total+= resulting_num;
+      running_total += resulting_num;
     }
-
   }
   std::cout << "Final result == " << running_total << "\n";
   return 0;
+}
 
+string replaceStringWithNum(string line)
+{
+  std::cout << "line in " << line << "\n";
+
+  map<string, string> number_map;
+  number_map["one"] = "o1e";
+  number_map["two"] = "t2o";
+  number_map["three"] = "th3ee";
+  number_map["four"] = "f4ur";
+  number_map["five"] = "f5ve";
+  number_map["six"] = "s6x";
+  number_map["seven"] = "s7ven";
+  number_map["eight"] = "e8ght";
+  number_map["nine"] = "n9e";
+
+  for (auto const &num : number_map){
+    std::size_t found = line.find(num.first);
+
+    if (found != std::string::npos){
+      line.replace(found, num.first.length(), num.second);
+    }
+  }
+  for (auto const &num : number_map){
+    std::size_t found = line.find(num.first);
+
+    if (found != std::string::npos){
+      line.replace(found, num.first.length(), num.second);
+    }
+  }
+
+  std::cout << "line out == " << line << "\n";
+
+  //std::size_t found = str.find(str2);
+  string final_line = line;
+  return final_line;
 }
