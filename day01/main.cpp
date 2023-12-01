ac55a2fb1a578cc2c9f5b8e1a15d78d2f5d83a55
Will Turner <will@thefutureforestcompany.com>
2023-12-01 17:02:01 +0000

    showing working.. going to store them as chars then combine then convert back to an int

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include<stdio.h>
+#include<ctype.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+  std::cout << "Opening file... \n";
+
+  fstream input_file;
+  input_file.open("inputs.txt", ios::in);
+
+  int running_total = 0;
+  
+  if (input_file.is_open()){
+    string line;
+
+    while(getline(input_file, line)){
+
+      std::cout << line << "\n";
+
+      int firstDigit = 0;
+      bool firstFound = false;
+      int lastDigit = 0;
+      bool lastFound = false;
+      
+      for (int i = 0; i < line.length(); i++){
+	if (!firstFound){
+	  if (isdigit(line[i])){
+	    char firstChar = line[i];
+	    firstDigit = firstChar - '0';
+	    firstFound = true;
+	  }
+	}
+      }
+
+      for (int i = line.length() -1 ; i >= 0; i--){
+	if (!lastFound){	
+	  if (isdigit(line[i])){
+	    char lastChar = line[i];
+	    lastDigit = lastChar - '0';
+	    lastFound = true;
+	  }
+	}
+      }
+      std::cout << "first digit found is " << firstDigit << "\n";
+      std::cout << "last digit found is " << lastDigit << "\n";
+
+    }
+
+    // combine the first and last to one number
+    
+    
+  }
+
+  return 0;
+
 }
